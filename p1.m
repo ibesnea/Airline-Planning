e0d0edb09bacb808c8c57524c5037a1f0e17a571
claudia <claudia.m.raducanu@gmail.com>
2017-11-30 11:58:02 +0100

    Renamed Excel no cost included

--- p1.m
+++ p1.m
@@ -1,3 +1,17 @@
 %% Initialization
-    addpath('/Users/Claudia/Documents/MSc-1/Q2/AE4423 Airline Planning and Optimization/Assignment')
- 
+    addpath('/Users/Claudia/Documents/MSc-1/Q2/AE4423/Airline-Planning')
+%% Read Data
+    filename = 'group11.xlsx';
+    lat  = xlsread(filename,'C6:Z6')*pi/180;
+    long = xlsread(filename,'C7:Z7')*pi/180;
+    R_e = 6371 %km  Earth Radius 
+    
+    for i=1:(size(lat))(2)
+        for j=1:(size(long))(2)
+            if i==j
+                d(i,j) = 0; 
+            else 
+                d(i,j) = (sin((lat(i)-lat(j))/2))^2
+            end      
+        end
+    end
