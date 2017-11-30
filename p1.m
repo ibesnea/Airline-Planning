e9412fd86e0207e6db7fa659c734d4a29e1b00e3
claudia <claudia.m.raducanu@gmail.com>
2017-11-30 12:14:00 +0100

    Distance not working

--- p1.m
+++ p1.m
@@ -1,17 +1,19 @@
 %% Initialization
     addpath('/Users/Claudia/Documents/MSc-1/Q2/AE4423/Airline-Planning')
 %% Read Data
-    filename = 'group11.xlsx';
+    filename = 'group111.xlsx';
     lat  = xlsread(filename,'C6:Z6')*pi/180;
     long = xlsread(filename,'C7:Z7')*pi/180;
-    R_e = 6371 %km  Earth Radius 
+    R_e = 6371; %km  Earth Radius 
     
-    for i=1:(size(lat))(2)
-        for j=1:(size(long))(2)
-            if i==j
-                d(i,j) = 0; 
-            else 
-                d(i,j) = (sin((lat(i)-lat(j))/2))^2
-            end      
+    szlat = size(lat);
+    szlong = size(long);
+    
+    for i=1:szlat(2)
+        for j=1:szlong(2)
+           arc(i,j) = 2* asin((sin((lat(i)-lat(j)))/2)^2+ ...
+                    cos(lat(i))*cos(lat(j)) *(sin((long(i))-long(j))/2)^2);
         end
     end
+    
+    d = arc*R_e; 
