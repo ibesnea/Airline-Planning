44bdd7f37054ec0bed2a25ee264951a7c2d74000
claudia <claudia.m.raducanu@gmail.com>
2017-11-30 14:59:43 +0100

    Distance calculation into function

---
+++ distance.m
@@ -0,0 +1,27 @@
+%% This function determines the distance between to airports i and j.
+%   Input:
+%       lat     = Latitude of airport;
+%       long    = longitude of airport;
+%       R_e     = Radius of the Earth;
+%   Variables:
+%       i= origin airport;
+%       j= destination airport; 
+%   Output:
+%       d= distance between airport i and j; 
+%--------------------------------------------------------------------------
+function d = distance(filename)
+    
+    lat  = xlsread(filename,'C6:Z6')*pi/180; % Convert lat to deg;
+    long = xlsread(filename,'C7:Z7')*pi/180; % Convert long to deg;
+    R_e = 6371; 
+    
+    szlat = size(lat);  % Number of airports; 
+    
+    for i=1:szlat(2)   
+        for j=1:szlat(2)
+           d(i,j) = round(2*asin(((sin((lat(i)-lat(j)))/2)^2+ ...
+                    cos(lat(i))*cos(lat(j)) * ...
+                    (sin((long(i))-long(j))/2)^2)^0.5)*R_e,2);
+        end
+    end    
+end
--- p1.m
+++ p1.m
@@ -4,19 +4,5 @@
     clc
     close all
 %% Read Data
-    filename = 'group11.xlsx';
-    lat  = xlsread(filename,'C6:Z6')*pi/180;
-    long = xlsread(filename,'C7:Z7')*pi/180;
-    R_e = 6371; %km  Earth Radius 
-    
-    szlat = size(lat);
-    szlong = size(long);
-    
-    for i=1:szlat(2)
-        for j=1:szlong(2)
-           d(i,j) = round(2*asin(((sin((lat(i)-lat(j)))/2)^2+ ...
-                    cos(lat(i))*cos(lat(j)) * ...
-                    (sin((long(i))-long(j))/2)^2)^0.5)*R_e,2);
-        end
-    end  
+    d = distance('group11.xlsx');
     
