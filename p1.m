fa8f3f34420e3d52158e1e8bb3878f3a9f57f16e
claudia <claudia.m.raducanu@gmail.com>
2017-11-30 14:48:46 +0100

    distance calculation

--- p1.m
+++ p1.m
@@ -1,7 +1,10 @@
 %% Initialization
     addpath('/Users/Claudia/Documents/MSc-1/Q2/AE4423/Airline-Planning')
+    clearvars
+    clc
+    close all
 %% Read Data
-    filename = 'group111.xlsx';
+    filename = 'group11.xlsx';
     lat  = xlsread(filename,'C6:Z6')*pi/180;
     long = xlsread(filename,'C7:Z7')*pi/180;
     R_e = 6371; %km  Earth Radius 
@@ -11,9 +14,9 @@
     
     for i=1:szlat(2)
         for j=1:szlong(2)
-           arc(i,j) = 2* asin((sin((lat(i)-lat(j)))/2)^2+ ...
-                    cos(lat(i))*cos(lat(j)) *(sin((long(i))-long(j))/2)^2);
+           d(i,j) = round(2*asin(((sin((lat(i)-lat(j)))/2)^2+ ...
+                    cos(lat(i))*cos(lat(j)) * ...
+                    (sin((long(i))-long(j))/2)^2)^0.5)*R_e,2);
         end
-    end
+    end  
     
-    d = arc*R_e; 
